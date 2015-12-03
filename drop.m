3da4718aab0f54672ccd4b634fe7020d0e6a3283
Jakob S. Joergensen <jakj@dtu.dk>
2015-12-03 08:23:52 +0000

    Removed Afun_matrix subfunction from SIRT methods, because is now regular function.

--- drop.m
+++ drop.m
@@ -589,16 +589,3 @@ while ~stop
     
 end
 X = X(:,1:l);
-
-%---------------------- Subfunction --------------------------
-function y = Afun_matrix(x,transp_flag,A)
-switch transp_flag
-    case 'size'
-        y = size(A);
-    case 'notransp'
-        y = A*x;
-    case 'transp'
-        y = A'*x;
-    otherwise
-        error('transp_flag must be ''size'', ''notransp'' or ''transp''')
-end
