59505586ba7740a84fe882ff750ea570e38d5df4
Jakob S. Joergensen <jakj@dtu.dk>
2017-02-15 14:05:33 +0000

    Added comment that for matrix-free, entries of system matrix are assumed to be nonnegative.

--- sart_wrapsirt.m
+++ sart_wrapsirt.m
@@ -6,5 +6,11 @@ function [X,info,restart] = sart_wrapsirt(varargin)
 % potentially change the largest singular value away from the known value
 % of 1, thus prompting need to compute it numerically, which is now a
 % feature of SART to avoid this potentially expensive computation.
+%
+% Note that the matrix-free version of SART assumes that all entries in the
+% system matrix are nonnegative. Using a system matrix with negative
+% entries leads to undefined behavior. In the case of providing the matrix
+% explicitly, negative entries are allowed and handled, but this is not
+% possible without explicit access to system matrix entries.
 
 [X,info,restart] = sirt('sart',varargin{:});
